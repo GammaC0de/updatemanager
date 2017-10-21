3fa9b0488f559b62d06e9223783565694b8295aa
GammaC0de <gammac0de@users.noreply.github.com>
2017-10-22 01:42:35 +0300

    Update

--- logger.php
+++ logger.php
@@ -1,8 +1,9 @@
 <?php
-print("Logger started.\n");
-ob_start();
-var_dump($_GET);
-var_dump($_POST);
-$result = ob_get_clean();
-print($result . "\n");
+print('Logger started.\n');
+print ('$_GET = ');
+print_r($_GET);
+print('\n');
+print ('$_POST = ');
+print_r($_POST);
+print('\n');
 ?>
