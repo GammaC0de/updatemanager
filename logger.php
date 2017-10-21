af103666f278c8eec2a62c42e433bf76ddaba6f6
GammaC0de <gammac0de@users.noreply.github.com>
2017-10-22 02:39:53 +0300

    Update

--- logger.php
+++ logger.php
@@ -2,5 +2,8 @@
 file_put_contents('php://stderr', "Logger started.\n");
 file_put_contents('php://stderr', '$_GET = ' . print_r($_GET, TRUE). "\n");
 file_put_contents('php://stderr', '$_POST = ' . print_r($_POST, TRUE). "\n");
-file_put_contents('php://stderr','Body = ' . file_get_contents('php://stdin') . "\n");
+if (ftell(STDIN) != false)
+    file_put_contents('php://stderr','Body = ' . file_get_contents('php://stdin') . "\n");
+else
+    file_put_contents('php://stderr', "No input\n");
 ?>
