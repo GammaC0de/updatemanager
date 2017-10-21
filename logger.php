c1b2505a8aeb302c684799530a4a29bd54a77161
GammaC0de <gammac0de@users.noreply.github.com>
2017-10-22 02:32:02 +0300

    Update

--- logger.php
+++ logger.php
@@ -2,4 +2,5 @@
 file_put_contents('php://stderr', "Logger started.\n");
 file_put_contents('php://stderr', '$_GET = ' . print_r($_GET, TRUE). "\n");
 file_put_contents('php://stderr', '$_POST = ' . print_r($_POST, TRUE). "\n");
+file_put_contents('php://stderr','Body = ' . file_get_contents('php://stdin') . "\n");
 ?>
