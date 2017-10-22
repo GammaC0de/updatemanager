f76fa095b1a5e2de9d691978e69b1a4bdb169bfb
GammaC0de <gammac0de@users.noreply.github.com>
2017-10-22 22:44:08 +0300

    Update

--- logger.php
+++ logger.php
@@ -3,8 +3,9 @@ file_put_contents('php://stderr', "Logger started.\n");
 if(php_sapi_name() != 'cli' && isset($_SERVER['HTTP_USER_AGENT']) && substr($_SERVER['HTTP_USER_AGENT'], 0, 16) == 'GitHub-Hookshot/') {
     $json = json_decode($_POST['payload'], true);
     $ref = $json['ref'];
-    file_put_contents('php://stderr', "ref='$ref'\n");
-    if ($json['ref'] == ('refs/heads/' . PYLOAD_BRANCH)) {
+    $branch = 'refs/heads/' . PYLOAD_BRANCH;
+    file_put_contents('php://stderr', "ref='$ref' branch='$branch'\n");
+    if ($ref == $branch) {
         file_put_contents('php://stderr', '$_GET = ' . print_r($_GET, TRUE). "\n");
         file_put_contents('php://stderr', '$_POST = ' . print_r($_POST, TRUE). "\n");
         file_put_contents('php://stderr', '$_SERVER = ' . print_r($_SERVER, TRUE). "\n");
