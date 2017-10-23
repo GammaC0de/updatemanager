6b3a4bf91eb6bf7f9695a95fedff77e34c205914
GammaC0de <gammac0de@users.noreply.github.com>
2017-10-23 16:27:34 +0300

    Update

--- updater.php
+++ updater.php
@@ -9,16 +9,35 @@ $starttime = time();
 $l = null; // Heroku has ephemeral filesystem
 //$l = new Katzgrau\KLogger\Logger(LOGDIR);
 
-//if(php_sapi_name() != 'cli' && ((!isset($_GET['key'])) || (hash("sha256", trim($_GET['key'])) != '1a82915eac2eead7c2ea4ccd8e0517908ff2318f64da33df5c19f5d967f088ae'))) {
-//    $l->warning('Invalid key');
-//    exit('Invalid key. Exiting.');
-//}
+if (php_sapi_name() != 'cli') {
+    if (isset($_SERVER['HTTP_USER_AGENT']) && substr($_SERVER['HTTP_USER_AGENT'], 0, 16) == 'GitHub-Hookshot/')
+        $payload = $_POST['payload'];
+    else
+        $payload = null;
 
-if(php_sapi_name() != 'cli' && isset($_SERVER['HTTP_USER_AGENT']) && substr($_SERVER['HTTP_USER_AGENT'], 0, 16) == 'GitHub-Hookshot/') {
-    $json = json_decode($_POST['payload'], true);
-    if (!isset($json['ref']) || $json['ref'] != 'refs/heads/' . PYLOAD_BRANCH) {
-        print("Not our branch<br>\n");
-        exit(0);
+    if (isset($_GET['key']))
+        if (hash("sha256", trim($_GET['key'])) != '1a82915eac2eead7c2ea4ccd8e0517908ff2318f64da33df5c19f5d967f088ae') {
+            //    $l->warning('Invalid key');
+            exit('Invalid Signature. Exiting.');
+        }
+    elseif ($payload != null) {
+        if (hash_hmac('sha1', $payload, getenv('GITHUB_SECRET')) != $_SERVER['HTTP_X_HUB_SIGNATURE']) {
+            //    $l->warning('Invalid key');
+            exit('Invalid Signature. Exiting.');
+        }
+    }
+    else {
+        //    $l->warning('Invalid key');
+        exit('Invalid Signature. Exiting.');
+    }
+
+    if ($payload != null) {
+        $json = json_decode($_POST['payload'], true);
+        if (!isset($json['ref']) || $json['ref'] != 'refs/heads/' . PYLOAD_BRANCH) {
+            //    $l->info('Not our branch');
+            print("Not our branch<br>\n");
+            exit(0);
+        }
     }
 }
 
