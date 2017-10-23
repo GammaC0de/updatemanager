045232cdbaa5d7eb6032f77b68e2ffc45731fac1
GammaC0de <gammac0de@users.noreply.github.com>
2017-10-23 23:32:38 +0300

    Update

--- updater.php
+++ updater.php
@@ -10,16 +10,24 @@ $l = null; // Heroku has ephemeral filesystem
 //$l = new Katzgrau\KLogger\Logger(LOGDIR);
 
 if (php_sapi_name() != 'cli') {
-    if (isset($_SERVER['HTTP_USER_AGENT']) && substr($_SERVER['HTTP_USER_AGENT'], 0, 16) == 'GitHub-Hookshot/')
+    if (isset($_SERVER['HTTP_USER_AGENT']) && substr($_SERVER['HTTP_USER_AGENT'], 0, 16) == 'GitHub-Hookshot/') {
+        if (!isset($_SERVER['HTTP_X_GITHUB_EVENT']) || $_SERVER['HTTP_X_GITHUB_EVENT'] != 'push') {
+            //    $l->info('Not a push event');
+            print("Not a push event<br>\n");
+            exit(0);
+        }
+
         $payload = $_POST['payload'];
+    }
     else
         $payload = null;
 
-    if (isset($_GET['key']))
+    if (isset($_GET['key'])) {
         if (hash("sha256", trim($_GET['key'])) != '1a82915eac2eead7c2ea4ccd8e0517908ff2318f64da33df5c19f5d967f088ae') {
             //    $l->warning('Invalid Signature');
             exit('Invalid Signature. Exiting.');
         }
+    }
     elseif ($payload != null) {
         if (hash_hmac('sha1', $payload, getenv('GITHUB_SECRET')) != $_SERVER['HTTP_X_HUB_SIGNATURE']) {
             //    $l->warning('Invalid Signature');
