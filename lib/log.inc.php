aa90870065a22a0d849265ff3e524dfaa855bc4d
GammaC0de <gammac0de@users.noreply.github.com>
2017-10-26 02:31:39 +0300

    Update

--- lib/log.inc.php
+++ lib/log.inc.php
@@ -8,16 +8,28 @@ class Logger
 {
     private $l;
     private $cli;
+    private $dyno;
 
     function __construct()
     {
-        print ("php_sapi_name() =" . php_sapi_name());
         $this->cli = php_sapi_name() == 'cli';
-        $this->l = getenv('DYNO') == false  && !$this->cli ? new Katzgrau\KLogger\Logger(LOGDIR) : null;
+        $this->dyno = getenv('DYNO') != false;
+        $this->l = !$this->dyno  && !$this->cli ? new Katzgrau\KLogger\Logger(LOGDIR) : null;
+    }
+
+    function send_remote_syslog($message, $component = "updater", $program="updatemanager") {
+        $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        foreach(explode(PHP_EOL, $message) as $line) {
+            $syslog_message = "<22>" . date('M d H:i:s ') . $program . ' ' . $component . ': ' . $line;
+            socket_sendto($sock, $syslog_message, strlen($syslog_message), 0, getenv('RSYSLOG_SERVER'), getenv('RSYSLOG_PORT'));
+        }
+        socket_close($sock);
     }
 
     public function debug($msg) {
         print($msg . PHP_EOL);
+        if ($this->dyno)
+            $this->send_remote_syslog($msg);
         if (!$this->cli)
             if (is_null($this->l)) {
                 error_log($msg);
@@ -30,6 +42,8 @@ class Logger
 
     public function info($msg) {
         print($msg . PHP_EOL);
+        if ($this->dyno)
+            $this->send_remote_syslog($msg);
         if (!$this->cli)
             if (is_null($this->l)) {
                 error_log($msg);
@@ -42,6 +56,8 @@ class Logger
 
     public function warning($msg) {
         print($msg . PHP_EOL);
+        if ($this->dyno)
+            $this->send_remote_syslog($msg);
         if (!$this->cli)
             if (is_null($this->l)) {
                 error_log($msg);
@@ -54,6 +70,8 @@ class Logger
 
     public function error($msg) {
         print($msg . PHP_EOL);
+        if ($this->dyno)
+            $this->send_remote_syslog($msg);
         if (!$this->cli)
             if (is_null($this->l)) {
                 error_log($msg);
