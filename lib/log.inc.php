213bcdb015e0eb19ab9943dbfb5dce754eaadec0
GammaC0de <gammac0de@users.noreply.github.com>
2017-10-26 15:09:50 +0300

    Update

--- lib/log.inc.php
+++ lib/log.inc.php
@@ -8,18 +8,18 @@ define('RSYSLOG_PORT', getenv('RSYSLOG_PORT'));
 
 class Logger
 {
-    private $l;
+    private $klogger;
     private $cli;
-    private $dyno;
+    private $rsyslog;
 
     function __construct()
     {
         $this->cli = php_sapi_name() == 'cli';
-        $this->dyno = getenv('DYNO') != false;
-        $this->l = !$this->dyno  && !$this->cli ? new Katzgrau\KLogger\Logger(LOGDIR) : null;
+        $this->rsyslog = getenv('RSYSLOG_SERVER') != false && getenv('RSYSLOG_PORT') != false;
+        $this->klogger = !$this->rsyslog  && !$this->cli ? new Katzgrau\KLogger\Logger(LOGDIR) : null;
     }
 
-    function send_remote_syslog($message, $component = "updater", $program="updatemanager") {
+    function send_remote_syslog($message, $component = "updatemanager", $program="pyload") {
         $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
         foreach(explode(PHP_EOL, $message) as $line) {
             $syslog_message = "<22>" . date('M d H:i:s ') . $program . ' ' . $component . ': ' . $line;
@@ -30,34 +30,34 @@ class Logger
 
     public function debug($msg) {
         print($msg . PHP_EOL);
-        if ($this->dyno)
-            $this->send_remote_syslog($msg);
-        if (!is_null($this->l))
-                $this->l->debug($msg);
+        if ($this->rsyslog)
+            $this->send_remote_syslog("[DEBUG] " . $msg);
+        if (!is_null($this->klogger))
+                $this->klogger->debug($msg);
     }
 
     public function info($msg) {
         print($msg . PHP_EOL);
-        if ($this->dyno)
-            $this->send_remote_syslog($msg);
-        if (!is_null($this->l))
-                $this->l->info($msg);
+        if ($this->rsyslog)
+            $this->send_remote_syslog("[INFO] " . $msg);
+        if (!is_null($this->klogger))
+                $this->klogger->info($msg);
     }
 
     public function warning($msg) {
         print($msg . PHP_EOL);
-        if ($this->dyno)
-            $this->send_remote_syslog($msg);
-        if (!is_null($this->l))
-                $this->l->warning($msg);
+        if ($this->rsyslog)
+            $this->send_remote_syslog("[WARNING] " . $msg);
+        if (!is_null($this->klogger))
+                $this->klogger->warning($msg);
     }
 
     public function error($msg) {
         print($msg . PHP_EOL);
-        if ($this->dyno)
-            $this->send_remote_syslog($msg);
-        if (!is_null($this->l))
-                $this->l->error($msg);
+        if ($this->rsyslog)
+            $this->send_remote_syslog("[ERROR] " . $msg);
+        if (!is_null($this->klogger))
+                $this->klogger->error($msg);
     }
 }
 ?>
